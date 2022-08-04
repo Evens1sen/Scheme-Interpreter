f8d546628b03a68a80cc7d2d7a2d05b8e1279b6c
mhy <11912919@mail.sustech.edu.cn>
2022-08-04 16:25:28 -0700

    Finish q6

--- scheme_eval_apply.py
+++ scheme_eval_apply.py
@@ -95,7 +95,14 @@ def eval_all(expressions, env):
     2
     """
     # BEGIN PROBLEM 6
-    return scheme_eval(expressions.first, env)  # replace this with lines of your own code
+    if expressions == nil:
+        return None
+    
+    if expressions.rest == nil:
+        return scheme_eval(expressions.first, env)
+
+    scheme_eval(expressions.first, env)
+    return eval_all(expressions.rest, env)
     # END PROBLEM 6
 
 
