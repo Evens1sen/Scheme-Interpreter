e87b7c6fd4c13ae29ed1d66a6f3be9c9387033a5
mhy <11912919@mail.sustech.edu.cn>
2022-08-05 15:15:07 -0700

    Finish q16

--- questions.scm
+++ questions.scm
@@ -22,7 +22,12 @@
 ;; the merged lists.
 (define (merge ordered? list1 list2)
   ; BEGIN PROBLEM 16
-  'replace-this-line
+  (cond
+    ((null? list1) list2)
+    ((null? list2) list1)
+    ((ordered? (car list1) (car list2)) (cons (car list1) (merge ordered? (cdr list1) list2)))
+    (else (cons (car list2) (merge ordered? list1 (cdr list2))))
   )
+)
   ; END PROBLEM 16
 
